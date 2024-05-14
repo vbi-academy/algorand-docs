3f4d3aa8048d46e90bf890bdcebb0003792dfe25
jayden-dang <jayden@jaydendang.com>
2024-05-14 23:43:17 +0700

    fix: docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,10 +25,10 @@ const config: Config = {
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
-  // i18n: {
-  //   defaultLocale: 'en',
-  //   locales: ['en'],
-  // },
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
   plugins: [tailwindPlugin],
 
   presets: [
