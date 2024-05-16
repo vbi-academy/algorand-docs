3835d998c2cb885e264b7b2e9143a473a198d362
jayden-dang <jayden@jaydendang.com>
2024-05-16 15:21:32 +0700

    fix: change docs direct

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/">
+            to="/basic/what-is-blockchain-and-it-benefits">
             Algorand Document
           </Link>
         </div>
