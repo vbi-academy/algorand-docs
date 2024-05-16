8afaefce1dbad0f347f34435ac6cd12a9e6c2287
jayden-dang <jayden@jaydendang.com>
2024-05-16 16:18:57 +0700

    feat: add section 2345

--- sidebars.ts
+++ sidebars.ts
@@ -18,24 +18,51 @@ const sidebars: SidebarsConfig = {
       type: 'category',
       label: 'Introduction Algorand',
       items: [
-        'basic/introduction-algorand/insight-into-algorand',
-        'basic/introduction-algorand/ecosystem-overview',
-        'basic/introduction-algorand/insight-into-avm-evm'
+        {
+          type: 'doc',
+          id: 'basic/introduction-algorand/insight-into-algorand',
+        },
+        {
+          type: 'doc',
+          id: 'basic/introduction-algorand/the-big-picture'
+        },
+        {
+          type: 'doc',
+          id: 'basic/introduction-algorand/ecosystem-overview',
+        },
+        {
+          type: 'doc',
+          id: 'basic/introduction-algorand/insight-into-avm-evm',
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Setup Development Environment',
+      items: [
+        'basic/setup/macos',
+        'basic/setup/linux',
+        'basic/setup/windows',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Development Tools & Extensions',
+      items: [
+        'basic/tools/development-tools',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'First Your Smart Contract',
+      items: [
+        'basic/first-smart-contract/smart-contract-structure-python',
       ],
     },
   ],
   advancedSidebar: [
     'advanced/advanced'
   ],
-
-  // tutorialSidebar: [
-  //   'first-smart-contract',
-  //   {
-  //     type: 'category',
-  //     label: 'Tutorial',
-  //     items: ['introduction-near/what-is-near'],
-  //   },
-  // ],
 };
 
 export default sidebars;
